c526b8391f359fff0469ee81e368a7a1efbdd037
andrei <andrei.ionita@rwth-aachen.de>
2018-10-22 15:50:07 +0200

    full cleanup

--- workspace/eppoc/src/prediction/csvprocessing/ReadCSV.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 07.05.2017
-
-@author: andigenu
-'''
-import pandas as pd
-
-data = pd.read_csv('C:\\Users\\andigenu\\parking\\sfpark\\Meters_20130412.csv', sep=',')
-print(data)
--- workspace/eppoc/src/tryouts/LargestArrayPossible.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 06.05.2017
-
-@author: andigenu
-'''
-import numpy as np
-
-matrix = np.zeros((2,100000,400))
-print(matrix)
